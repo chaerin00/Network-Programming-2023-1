0816b56f6ae7d9a8d3d39b44d096e912a689a656
chaerin <achrvvv@gmail.com>
2023-03-17 15:26:11 +0900

    review udp chapter1

--- chapter2/udp_local.py
+++ chapter2/udp_local.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
-# Foundations of Python Network Programming, Third Edition
-# https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter02/udp_local.py
-# UDP client and server on localhost
-
-import argparse, socket
+import argparse
+import socket
 from datetime import datetime
 
 MAX_BYTES = 65535
+
 
 def server(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -20,15 +17,17 @@ def server(port):
         data = text.encode('ascii')
         sock.sendto(data, address)
 
+
 def client(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     text = 'The time is {}'.format(datetime.now())
     data = text.encode('ascii')
     sock.sendto(data, ('127.0.0.1', port))
     print('The OS assigned me the address {}'.format(sock.getsockname()))
-    data, address = sock.recvfrom(MAX_BYTES)  # Danger! See Chapter 2
+    data, address = sock.recvfrom(MAX_BYTES)
     text = data.decode('ascii')
     print('The server {} replied {!r}'.format(address, text))
+
 
 if __name__ == '__main__':
     choices = {'client': client, 'server': server}
