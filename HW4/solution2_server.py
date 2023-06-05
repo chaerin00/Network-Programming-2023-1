331524c65188456461fb804accaae44ae48c9e20
chaerin <achrvvv@gmail.com>
2023-06-05 17:46:03 +0900

    feat: add member image

--- HW4/solution2_server.py
+++ HW4/solution2_server.py
@@ -20,11 +20,14 @@ def scrape_member_details(url):
     soup = BeautifulSoup(response.content, 'html.parser')
 
     rows = []
-
     # it contains one or two members
     members = soup.find_all(class_="mYVXT")
     for member in members:
-        images = member.find_all('img')
+        image_elements = member.find_all('img')
+        images = []
+        for image in image_elements:
+            images.append(image['src'])
+
         paragraphs = member.find_all(class_="hJDwNd-AhqUyc-II5mzb")
         for p in paragraphs:
             text = p.text
@@ -39,18 +42,20 @@ def scrape_member_details(url):
                     text = text[1]
                 else:
                     text = text[0]
-                end_year, text = text.split(')', 1)
+                end_year, text = text.split(")", 1)
+                end_year = None if len(end_year) == 0 else end_year
                 interests = None
                 current_job_role = None
                 if "Research Interests:" in text:
                     _, interests = text.split('Research Interests:', 1)
                 elif "@" in text:
                     current_job_role, _ = text.split('@', 1)
-
-                print(name, role, start_year, end_year, interests, current_job_role)
+                row = [name, role, start_year, end_year, interests, current_job_role]
+                if len(images) > 0:
+                    row.append(images.pop())
+                rows.append(row)
 
     save_csv_data(rows)
-
     return 'success'
 
 
